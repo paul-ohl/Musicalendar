0d22a0fc8644f19a9c9997815dcb703d7e32e73a
Paul OHL <paul.lv.ohl@gmail.com>
2023-11-05 00:16:24 +0100

    Replaced the Yew project with a svelte one. I am probably going to edit this a bit in the future

---
+++ frontend/src/routes/homepage.svelte
@@ -0,0 +1,37 @@
+<header id="Header" class="section">
+	<h1 class="button is-primary">Musicalendar.</h1>
+	<div class="container is-secondary">
+		<p>
+			A spotify-connected app to see a visual representation of your music tastes over the years
+		</p>
+	</div>
+</header>
+
+<section id="action-buttons" class="section">
+	<button>Select with your account</button>
+	<p>or</p>
+	<form>
+		<input placeholder="Paste link to a playlist" />
+		<button>Go</button>
+	</form>
+</section>
+
+<section id="share" class="section">
+	<div class="container is-secondary">
+		<p>Share Musicalendar with your friends and collegues!</p>
+		<div class="container is-secondary">
+			<button>Fb</button>
+			<button>X</button>
+			<button>Lkdn</button>
+		</div>
+	</div>
+</section>
+
+<section id="more-info" class="section">
+	<p>
+		Musicalendar is a personal project of mine. The idea was sparked by my girlfriend having an
+		"audio journal" playlist, and wanting a cool way to visualize her year in music. My name is
+		Paul, I am a developer from France and you can find me online at paulohl.fr and on LinkedIn.
+		Also this project is open-source so you can checkout the code on github!
+	</p>
+</section>
