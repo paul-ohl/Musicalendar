757c5cd0964ba7287fa097b64a84424cacf5c146
Paul OHL <paul.lv.ohl@gmail.com>
2023-11-05 00:00:31 +0100

    Started writing the html for the homepage

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -3,16 +3,63 @@ use yew::prelude::*;
 #[function_component]
 fn App() -> Html {
     html! {
-    <section class="section">
+    <>
+    <section id="action-buttons" class="section">
         <div class="container">
             <h1 class="button is-primary">
-                {"Hello World"}
+                { "Musicalendar." }
             </h1>
-            <p class="subtitle is-warning">
-                {"My first website with "} <strong>{"Bulma"}</strong> {"!"}
-            </p>
+            <div class="container is-secondary">
+                <p>
+                    { "A spotify-connected app to see a visual representation of your music tastes over the years" }
+                </p>
+            </div>
         </div>
     </section>
+    <section id="action-buttons" class="section">
+        <button>
+            { "Select with your account" }
+        </button>
+        <p>{ "or" }</p>
+        <form>
+            <input placeholder="Paste link to a playlist"/>
+            <button>
+                { "Go" }
+            </button>
+        </form>
+    </section>
+    <section id="share-section" class="section">
+        <div class="container is-secondary">
+            <p>
+                { "Share Musicalendar with your friends and collegues!" }
+            </p>
+            <div class="container is-secondary">
+                <button>
+                    { "Fb" }
+                </button>
+                <button>
+                    { "X" }
+                </button>
+                <button>
+                    { "Lkdn" }
+                </button>
+            </div>
+        </div>
+    </section>
+    <section id="more-info" class="section">
+        <p>
+            {r#"
+Musicalendar is a personal project of mine.
+The idea was sparked by my girlfriend having an "audio journal" playlist, and
+wanting a cool way to visualize her year in music.
+My name is Paul, I am a developer from France and you can find me online at paulohl.fr
+and on LinkedIn.
+
+Also this project is open-source so you can checkout the code on github!
+            "#}
+        </p>
+    </section>
+    </>
     }
 }
 
